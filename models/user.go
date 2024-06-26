1fb4f5e10cdaf6f095cbcd3b8a9807ea4bad0292
fabienchevalier <97484369+fabienchevalier@users.noreply.github.com>
2024-06-26 20:48:46 +0200

    feat(init): basic user crud

---
+++ models/user.go
@@ -0,0 +1,12 @@
+package models
+
+type User struct {
+    ID    uint   `json:"id"`
+    Name  string `json:"name"`
+    Email string `json:"email"`
+}
+
+type CreateUserRequest struct {
+    Name  string `json:"name"`
+    Email string `json:"email"`
+}
